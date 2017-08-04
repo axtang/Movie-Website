a6a43c78031751f0929f78981727664306ad12e8
axtang <tangannabelle1@gmail.com>
2017-08-04 13:16:23 -0700

    A Simple Movie Website 4 August 2017
    There is still room for CSS work and adding Google Maps API...

---
+++ Movie Website/instancemovie.py
@@ -0,0 +1,115 @@
+import classmovie
+import fresh_tomatoes
+
+# Instances of which constructors will work on
+jeux_d_enfant = classmovie.Projectmovie(
+    "Jeux d'enfant",
+    "Two young would-be lovers as they go from childhood to adulthood \
+    with themselves as the greatest hurdle to their own happiness.",
+    "2003",
+    "Drama film/Romance",
+    "1h 33m",
+    "https://upload.wikimedia.org/wikipedia/en/e/e7/Love_Me_If_You_Dare_movie.jpg",
+    "https://www.youtube.com/watch?v=CX23hgYDTwg")
+
+pride_and_prejudice = classmovie.Projectmovie(
+    "Pride and Prejudice",
+    "Sparks fly when spirited Elizabeth Bennet meets single, rich, and proud \
+    Mr. Darcy. But Mr. Darcy reluctantly finds himself falling in love with a \
+    woman beneath his class. Can each overcome their own pride and prejudice?",
+    "2005",
+    "Drama film/Romance",
+    "2h 9min",
+    "https://images-na.ssl-images-amazon.com/images/M/MV5BMTA1NDQ3NTcyOTNeQTJeQWpwZ15BbWU3MDA0MzA4MzE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    "https://www.youtube.com/watch?v=fJA27Jujzq4")
+
+jane_eyre = classmovie.Projectmovie(
+    "Jane Eyre",
+    "Orphaned into the household of her Aunt Reed at Gateshead and subject \
+    to the cruel regime at Lowood charity school, Jane Eyre nonetheless \
+    emerges unbroken in spirit and integrity. She takes up the post of \
+    governess at Thornfield, falls in love with Mr. Rochester, and \
+    discovers the impediment to their lawful marriage in a story that \
+    transcends melodrama to portray a woman's passionate search for a \
+    wider and richer life than Victorian society traditionally allowed.",
+    "2011",
+    "Drma film/Romance",
+    "2h",
+    "https://images-na.ssl-images-amazon.com/images/M/MV5BNjU0Mjc0NzU3NF5BMl5BanBnXkFtZTcwMTU4OTkwNA@@._V1_UY1200_CR89,0,630,1200_AL_.jpg",
+    "https://www.youtube.com/watch?v=e8PLpXvhtlc")
+
+the_pianist = classmovie.Projectmovie(
+    "The Pianist",
+    "A Polish Jewish musician struggles to survive the destruction of \
+    the Warsaw ghetto of World War II.",
+    "2002",
+    "Drama film/Biography",
+    "2h 30min",
+    "https://moviran2.in/wp-content/uploads/393685.jpg",
+    "https://www.youtube.com/watch?v=CIRLLPa-j9o")
+
+memoirs_of_a_geisha = classmovie.Projectmovie(
+    "Memoirs of a Geisha",
+    "Nitta Sayuri reveals how she transcended her fishing-village \
+    roots and became one of Japan's most celebrated geisha.",
+    "2005",
+    "Drama film/Romance",
+    "2h 25min",
+    "https://images-na.ssl-images-amazon.com/images/I/51slE-07opL._SY445_.jpg",
+    "https://www.youtube.com/watch?v=4L-xlmakQvc")
+
+northanger_abbey = classmovie.Projectmovie(
+    "Northanger Abbey",
+    "A young woman's penchant for sensational Gothic novels \
+    leads to misunderstandings \
+    in the matters of the heart.",
+    "2007",
+    "Television film/Drama",
+    "2h",
+    "https://upload.wikimedia.org/wikipedia/en/c/c9/Northanger_Abbey.jpg",
+    "https://www.youtube.com/watch?v=gf92N9g_aEM")
+
+interstellar = classmovie.Projectmovie(
+    "Interstellar",
+    "With our time on Earth coming to an end, a team \
+    of explorers undertakes the most important mission in \
+    human history; traveling beyond this galaxy to discover whether \
+    mankind has a future among the stars. ",
+    "2014",
+    "Mystery/Science fiction film",
+    "2h 49min",
+    "https://bfox.files.wordpress.com/2014/11/interstellar-movie.jpg",
+    "https://www.youtube.com/watch?v=2LqzF5WauAw")
+
+haute_cuisine = classmovie.Projectmovie(
+    "Haute Cousine",
+    "The story of Daniele Delpeuch and how she was appointed as \
+    the private chef for Francois Mitterrand.",
+    "2012",
+    "Biography, comedy",
+    "1h 35min",
+    "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQzMDUzODc3M15BMl5BanBnXkFtZTcwNTA4NzQzOQ@@._V1_SY1000_CR0,0,667,1000_AL_.jpg",
+    "https://www.youtube.com/watch?v=OSaZ7xyzOGo")
+
+beatriz_at_dinner = classmovie.Projectmovie(
+    "Beatriz at Dinner",
+    "Beatriz is a Los Angeles massage therapist and holistic healer \
+    who drives to the seaside \mansion of her client Cathy. When her \
+    old Volkswagen breaks down, she receives a friendly invitation \
+    from Cathy to stay for a seemingly innocent business dinner. As the \
+    guests arrive and the wine starts to flow, Beatriz finds herself \
+    in an escalating war of words with Doug Strutt, a ruthless real \
+    estate mogul who cares more about money than people.",
+    "2017",
+    "Drama film/comedy",
+    "1h 23min",
+    "http://t1.gstatic.com/images?q=tbn:ANd9GcREe0IrylJt1tVGYBfrpRLvaedoiGGaorZeph-c-a_gZYdSPIxV",
+    "https://www.youtube.com/watch?v=bCLNTmNj5bI")
+
+movies = [jeux_d_enfant, pride_and_prejudice, jane_eyre, the_pianist,
+          memoirs_of_a_geisha, northanger_abbey, interstellar, haute_cuisine,
+          beatriz_at_dinner]
+
+
+# call the method open_movies_page in fresh tomatoes
+fresh_tomatoes.open_movies_page(movies)
