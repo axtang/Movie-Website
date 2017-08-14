0ecba1a8faf1e9d3bb8619b0012931fbf9043dac
Annabelle Tang <tangannabelle1@gmail.com>
2017-08-14 08:53:35 -0700

    Updated format

--- Movie Website/classmovie.py
+++ Movie Website/classmovie.py
@@ -8,14 +8,14 @@ import webbrowser
 class Projectmovie():
     def __init__(
         self, title_movie, description_movie, year_movie, category_movie,
-        duration_movie, poster_movie, trailer_movie): 
-        self.title = title_movie
-        self.description = description_movie
-        self.year = year_movie
-        self.category = category_movie
-        self.duration = duration_movie
-        self.poster_image_url = poster_movie
-        self.trailer_youtube_url = trailer_movie
+            duration_movie, poster_movie, trailer_movie):
+            self.title = title_movie
+            self.description = description_movie
+            self.year = year_movie
+            self.category = category_movie
+            self.duration = duration_movie
+            self.poster_image_url = poster_movie
+            self.trailer_youtube_url = trailer_movie
 
-# this method opens the youtube movie trailer
+    # this method opens the youtube movie trailer
     def trailer_movie(): webbrowser.open(self.trailer_youtube_url)
